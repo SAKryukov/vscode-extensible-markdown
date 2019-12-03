e8622509fbc2f4e51a328c194adcf0f938cee203
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2019-12-03 18:50:01 -0500

    Replacements: complete

--- out/replacements.js
+++ out/replacements.js
@@ -2,22 +2,27 @@
 
 module.exports = (md, options) => {
 
-    const utility = require("./utility");
-
-    const abbreviationRegexp = new RegExp(/\{(.+?)\}/); // in **: *{Request for Comments}RFC*
     const patterns = [
-        { name: "fence language", regexpString: "\\{(.+?)\\}", type: "fence", field: "info", attribute: "lang", attributeValue: 1 },
-        { name: "class", regexpString: "\\{\\.(.+?)\\}", type: "paragraph_open", token: +1, field: "content", attribute: "class", attributeValue: 1 },
-        { name: "title", regexpString: "\\{\\^(.+?)\\}", type: "paragraph_open", token: +1, field: "content", attribute: "title", attributeValue: 1 },
-        { name: "attribute=value", regexpString: "\\{(.+?)\\=(.+?)\\}", type: "paragraph_open", token: +1, field: "content", attribute: 1, attributeValue: 2 },
-        { name: "article title", regexpString: "\\{title\\}", type: "paragraph_open", token: +1, nextTokenType: "inline", field: "content", attribute: "class", attributeValue: "title" },
+        { find: /!\=\=/g, replace: "≢" }, // !== not identical
+        { find: /!\=/g, replace: "≠" }, // !== not equal
+        { find: /!\|\|/g, replace: "∦" }, // !|| not parallel
+        { find: /!\~\~/g, replace: "≉" }, // !~~ not almost equal
+        { find: /<\=/g, replace: "≦" }, // <= less or equal
+        { find: /\>\=/g, replace: "≧" }, // >= more or equal
+        { find: /!\~/g, replace: "≁" }, // !~ not tilde
+        { find: /\-\+/g, replace: "∓" }, // -+
+        { find: /\-\:/g, replace: "÷" }, // -:
+        { find: /\|\|/g, replace: "∥" }, // || parallel
+        { find: /\~\~/g, replace: "≈" }, // ~~ almost equal
+        { find: /\=\=\=/g, replace: "≣" }, // === strictly equivalent
+        { find: /\=\=/g, replace: "≡" }, // == identical
+        { find: /!\.\</g, replace: "∉" }, // !.< not element of
+        { find: /\.\</g, replace: "∈" }, // .< element of
+        { find: /\<\|/g, replace: "⊂" }, // <| subset of
+        { find: /\|\>/g, replace: "⊃" }, // |> superset of
     ];
-    for (let pattern of patterns)
-        pattern.regexp = new RegExp(pattern.regexpString);
-    const tokenDictionary = {};
 
     md.core.ruler.after("replacements", "extra_replacements", state => {
-        const regexp = new RegExp(/\-\+/, "g");
         for (let index = state.tokens.length - 1; index >= 0; --index) {
             const token = state.tokens[index];
             if (token.type != "inline") continue;
@@ -30,8 +35,10 @@ module.exports = (md, options) => {
                     else if (childToken.type == "link_close")
                         insideAutolink += 1;
                     else if (childToken.type == "text") {
-                        if (!insideAutolink && regexp.exec(childToken.content))
-                                childToken.content = childToken.content.replace(regexp, "!!!");
+                        if (!insideAutolink)
+                            for (let pattern of patterns)
+                                if (pattern.find.exec(childToken.content))
+                                    childToken.content = childToken.content.replace(pattern.find, pattern.replace);
                     } //if
                 } //if
             } //loop

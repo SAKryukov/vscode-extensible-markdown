2466cfc63774c01e393b6f46c8c6f4f01c3861e8
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2019-12-07 22:48:46 -0500

    Fixed title locator regex

--- out/attribution.js
+++ out/attribution.js
@@ -12,7 +12,7 @@ module.exports = (md, options) => {
     const mergedAttribute = "class";
     const patterns = [
         { name: "class", regexp: /\{\.(.+?)\}/g, attribute: mergedAttribute, attributeValue: 1 },
-        { name: "document title", regexp: new RegExp(options.titleLocatorRegex, "g"), attribute: "class", attributeValue: "title", isDocumentTitlePattern: true },
+        { name: "document title", regexp: new RegExp(options.titleLocatorRegex), attribute: "class", attributeValue: "title", isDocumentTitlePattern: true },
         { name: "attribute=value", regexp: /\{([a-z]+?)\=(.+?)\}/g, attribute: 1, attributeValue: 2 },
     ];
     const blockPatterns = {
